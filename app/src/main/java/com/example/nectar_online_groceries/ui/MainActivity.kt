660340309a1c1b85bc5ac69c99390a177b77ed8d
sthefany.siqueira <sthefany.siqueira@g.globo>
2025-11-10 02:31:11 -0300

    fix: update code structure

---
+++ app/src/main/java/com/example/nectar_online_groceries/ui/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.nectar_online_groceries.ui
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.nectar_online_groceries.ui.theme.NectarOnlineGroceriesTheme
+import com.example.nectar_online_groceries.viewmodel.UserViewModel
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            val navController = rememberNavController()
+            val userViewModel: UserViewModel = viewModel()
+
+            NectarOnlineGroceriesTheme {
+                Scaffold(modifier = Modifier.Companion.fillMaxSize()) { innerPadding ->
+                    NavHost(navController = navController, startDestination = "signIn") {
+                        composable("signIn") {
+                            SignInScreen(
+                                viewModel = userViewModel,
+                                onEnterClick = { user ->
+                                    Log.i("MainActivity", "User signed in: $user")
+                                    navController.navigate("homeScreen") {
+                                        popUpTo("signIn") { inclusive = true }
+                                    }
+                                },
+                                onForgotClick = {
+                                    userViewModel.setErrorMessage("Link funcionando e redirecionamento feito!")
+                                },
+                                onRegisterClick = {
+                                    userViewModel.setErrorMessage("Link funcionando e redirecionamento feito!")
+                                }
+                            )
+                        }
+                        composable("homeScreen") {
+                            HomeScreen()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
