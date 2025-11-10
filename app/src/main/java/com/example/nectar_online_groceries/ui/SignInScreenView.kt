5d1576407bfa34390a72bc44ee92ce01253ba80e
sthefany.siqueira <sthefany.siqueira@g.globo>
2025-11-10 02:31:01 -0300

    fix: update code structure

--- app/src/main/java/com/example/nectar_online_groceries/ui/SignInScreenView.kt
+++ app/src/main/java/com/example/nectar_online_groceries/ui/SignInScreenView.kt
@@ -1,4 +1,4 @@
-package com.example.nectar_online_groceries.views
+package com.example.nectar_online_groceries.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
